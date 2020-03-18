f95e1bcff0c31b9183dce110dabbb10f3f5bcf9a
pr0d <pavel.erohovets@gmail.com>
2020-03-18 12:23:22 +0300

    happy birthday timer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DateCountdown from 'react-date-countdown-timer';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            ....
+       <DateCountdown dateTo='June 13, 2020 00:00:01' callback={()=>alert('Happy birthday')}/>
         </p>
       </header>
     </div>
